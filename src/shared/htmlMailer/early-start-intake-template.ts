dd00ddd0d036bdc44cf02b222b1c0a338caf9f65
Rajendra Kumar Majhi <rajendra@cybrain.co.in>
2022-01-20 18:45:23 +0530

    email template

--- src/shared/htmlMailer/early-start-intake-template.ts
+++ src/shared/htmlMailer/early-start-intake-template.ts
@@ -18,8 +18,8 @@ export var mailer = {
         `                <img src="https://childcarecrm.cyberschoolmanager.com/assets/images/logo_family.png">`+
         `            </div>`+
         `            <div style="float: right; width: 68%;text-align: right;margin-top: 25px;">`+
-        `                <h3 style="color: #0094df;font-size: 42px;margin: 0;">Exceptional Family Resource Center</h3>`+
-        `                <p style="color: #0094df;line-height: 29px;font-size: 24px;margin-top: 0;">`+
+        `                <h3 style="color: #0094df;font-size: 36px;margin: 0;">Exceptional Family Resource Center</h3>`+
+        `                <p style="color: #0094df;line-height: 29px;font-size: 23px;margin-top: 0;">`+
         `                    <i>A Center for Families of Children with Developmental Disabilities<br>`+
         `                        A 501 © 3 Not-for-Profit Public Charitable Organization</i>`+
         `                </p>`+
